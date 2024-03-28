9debc4d20a59a2482c6d1d32738727b9d25d4663
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2024-03-28 23:50:51 +0300

    Create main.dart

---
+++ main.dart
@@ -0,0 +1,26 @@
+//main.dart file
+
+import 'package:flutter/material.dart';
+import 'package:spcounter/app_main_page.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'App Opening Counter',
+      theme: ThemeData(
+  
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: AppOpeningCounter(),
+    );
+  }
+}
+
